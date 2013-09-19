74f9500fa81bc84afd6c4aec3c12885828391191
Paul <ogunlowo.p@gmail.com>
2013-09-19 19:26:44 -0400

    Deleted a space in the topic controller

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -33,18 +33,6 @@ class TopicController < ApplicationController
   def destroy
   end
 
-  def follow
-    @topic = Topic.find(params[:query])
-    @user = current_user
-    if users_topics.create(user_id: @user.id, topic_id: @topic.id)
-      flash[:notice] = "You have successfully followed this topic"
-      redirect_to root_path
-    else 
-      flash[:notice] = "There was a problem following that topic"
-      redirect_to root_path
-    end 
-  end 
-
   private 
 
   def topic_params
